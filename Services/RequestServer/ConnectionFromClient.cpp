e3e3c602e6cd3fec7ca970d79f9330905d0665e0
Suraj Yadav <ysuraj877@gmail.com>
2026-04-16 19:32:47 +0530

    Add sample debugs

--- Services/RequestServer/ConnectionFromClient.cpp
+++ Services/RequestServer/ConnectionFromClient.cpp
@@ -3,9 +3,16 @@
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
-
+#include <AK/HashMap.h>
+#include <AK/FixedArray.h>
+#include <AK/Format.h>
 #include <AK/IDAllocator.h>
 #include <AK/NonnullOwnPtr.h>
+#include <AK/Optional.h>
+#include <AK/SinglyLinkedList.h>
+#include <AK/String.h>
+#include <AK/StringView.h>
+#include <AK/Variant.h>
 #include <AK/WeakPtr.h>
 #include <LibCore/EventLoop.h>
 #include <LibCore/Proxy.h>
@@ -22,6 +29,7 @@
 #include <RequestServer/Request.h>
 #include <RequestServer/Resolver.h>
 #include <RequestServer/WebSocketImplCurl.h>
+#include <AK/DistinctNumeric.h>
 
 namespace RequestServer {
 
@@ -202,6 +210,45 @@ void ConnectionFromClient::set_use_system_dns()
 void ConnectionFromClient::start_request(u64 request_id, ByteString method, URL::URL url, Vector<HTTP::Header> request_headers, ByteBuffer request_body, HTTP::CacheMode cache_mode, HTTP::Cookie::IncludeCredentials include_credentials, Core::ProxyData proxy_data)
 {
     dbgln_if(REQUESTSERVER_DEBUG, "RequestServer: start_request({}, {})", request_id, url);
+
+    // Vector<StringView> vector;
+    // vector.append("RequestServer"sv);
+
+    // AK::SinglyLinkedList<StringView> list;
+    // list.append("RequestServer"sv);
+    // list.append("Hello"sv);
+    // list.append("World"sv);
+
+    // AK::Atomic<u8> counter { 77 };
+    // dbgln("Counter: {}", counter.load());
+
+    // [[maybe_unused]] AK::Array<u8, 5> bytes { 1, 2, 3, 4, 5 };
+
+    // FixedArray<StringView> array(FixedArray<StringView>::create(5).release_value());
+    // array[0] = "Hello"sv;
+    // array[1] = "World"sv;
+    // array[2] = "!"sv;
+    // array[3] = "How"sv;
+    // array[4] = "Are"sv;
+
+    // Optional<StringView> maybe_string;
+    // maybe_string = "Now I have a value"sv;
+
+    // Optional<u8> maybe_u8;
+    // maybe_u8 = 8;
+
+    // Optional<String> maybe_string2;
+    // maybe_string2 = MUST(String::from_utf8("Hello from String"sv));
+
+    // Variant<StringView, u8> variant = "Suraj"sv;
+
+    HashMap<StringView, int> counts {
+        { "alpha"sv, 1 },
+        { "beta"sv, 2 },
+        { "gamma"sv, 3 },
+        { "delta"sv, 4 },
+        { "epsilon"sv, 5 },
+    };
 
     auto request = Request::fetch(request_id, m_disk_cache, cache_mode, *this, m_curl_multi, m_resolver, move(url), move(method), HTTP::HeaderList::create(move(request_headers)), move(request_body), include_credentials, m_alt_svc_cache_path, proxy_data);
     m_active_requests.set(request_id, move(request));
